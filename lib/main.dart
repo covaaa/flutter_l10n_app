4040c074a1e54726c510c4cdcea94a1ee6b961e5
cova <cova8458@gmail.com>
2024-12-28 19:28:52 +0900

    add riverpod packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_l10n_app/src/core/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const App());
+void main() => runApp(const ProviderScope(child: App()));
--- test/extensions/tester_x.dart
+++ test/extensions/tester_x.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_l10n_app/src/l10n/l10n.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension WidgetTesterX on WidgetTester {
+  Future<void> pumpRiverpodWidget({
+    required Widget child,
+    List<Override> overrides = const [],
+  }) {
+    return pumpWidget(ProviderScope(overrides: overrides, child: child));
+  }
+
   Future<void> pumpApp({
     required Widget child,
+    List<Override> overrides = const [],
   }) async {
-    return pumpWidget(
-      MaterialApp(
+    return pumpRiverpodWidget(
+      overrides: overrides,
+      child: MaterialApp(
         supportedLocales: L10n.supportedLocales,
         localizationsDelegates: L10n.localizationsDelegates,
         home: child,
