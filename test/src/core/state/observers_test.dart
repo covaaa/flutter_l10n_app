df8b710d2107d0093c3823ab65c766c78aa8a35a
cova <cova8458@gmail.com>
2025-01-05 06:18:12 +0900

    update dependencies: bump drift_flutter to 0.2.4, very_good_analysis to 7.0.0

--- test/src/core/state/observers_test.dart
+++ test/src/core/state/observers_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
+// For mocking ProviderBase
 // ignore: subtype_of_sealed_class
 class MockProvider extends Mock implements ProviderBase<Object?> {}
 
